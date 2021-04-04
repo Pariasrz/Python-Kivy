0a08e60a78b965a3caaedba462b21b2cecfb987b
Paria Sarzaeim <59663526+Pariasrz@users.noreply.github.com>
2021-04-04 09:36:44 +0430

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,17 @@
 from kivy.app import App
+
+#label
 from kivy.uix.label import Label
 class firstapp(App):
     def build(self):
         return Label(text="Hello world", font_size=60)
+    
+firstapp().run()
+
+#button
+from kivy.uix.button import Button
+class firstapp(App):
+    def build(self):
+        return Button(text="Hello world", font_size=60)
     
 firstapp().run()
